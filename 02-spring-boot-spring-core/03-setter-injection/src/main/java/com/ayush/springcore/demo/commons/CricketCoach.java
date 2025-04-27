d658db13d74b1ec4c4ab2a179d79514c396708ad
Slypher <ayushgupta6719@gmail.com>
2025-04-27 23:19:00 +0530

    setter injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/ayush/springcore/demo/commons/CricketCoach.java
@@ -0,0 +1,10 @@
+package com.ayush.springcore.demo.commons;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+    public String getDailyWorkout() {
+        return "Practice throwing ball for 15 minutes";
+    }
+}

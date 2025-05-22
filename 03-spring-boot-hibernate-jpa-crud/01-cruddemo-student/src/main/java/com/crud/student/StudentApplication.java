37874ab06ce356707de7564f7c41914342342d18
Slypher <ayushgupta6719@gmail.com>
2025-05-22 08:11:23 +0530

    create

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/crud/student/StudentApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/crud/student/StudentApplication.java
@@ -1,5 +1,7 @@
 package com.crud.student;
 
+import com.crud.student.dao.StudentDAO;
+import com.crud.student.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,32 @@ public class StudentApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World!");
+//			createStudent(studentDAO);
+			createMultipleStudent(studentDAO);
 		};
+	}
+
+	private void createMultipleStudent(StudentDAO studentDAO) {
+		System.out.println("Creating 3 students");
+		Student student1 = new Student("Jeet", "Gupta", "jeet@gmail.com");
+		Student student2 = new Student("Jyoti", "Gupta", "jyoti@gmail.com");
+		Student student3 = new Student("Asmi", "Gupta", "asmi@gmail.com");
+
+		System.out.println("Saving students");
+		studentDAO.save(student1);
+		studentDAO.save(student2);
+		studentDAO.save(student3);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating a student");
+		Student student = new Student("Ayush", "Gupta", "ayushgupta6719@gmail.com");
+
+		System.out.println("Saving student into DB");
+		studentDAO.save(student);
+
+		System.out.println("student has id " + student.getId());
 	}
 }
---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/crud/student/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.crud.student.dao;
+
+import com.crud.student.entity.Student;
+
+public interface StudentDAO {
+    public void save(Student student);
+}

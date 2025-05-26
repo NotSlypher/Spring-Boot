cc90b983cad5c950dbecc16da3e60266dae242fd
Slypher <ayushgupta6719@gmail.com>
2025-05-26 20:29:20 +0530

    jpa repository

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-jpa-repository/src/main/java/com/ayush/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,44 @@
+package com.ayush.springboot.cruddemo.service;
+
+import com.ayush.springboot.cruddemo.dao.EmployeeRepository;
+import com.ayush.springboot.cruddemo.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+
+        Employee employee = null;
+        if(employeeOptional.isPresent()){
+            employee = employeeOptional.get();
+        }
+
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
+}

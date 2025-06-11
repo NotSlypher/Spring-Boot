2d0ee9fe7c9319f6876ef3338988232ce7658d77
Slypher <ayushgupta6719@gmail.com>
2025-06-11 16:21:11 +0530

    one to many uni

---
+++ 09-spring-boot-jpa-advanced-mappings/04-one-to-many-uni/src/main/java/com/spring/jpa_advanced/dao/AppDAO.java
@@ -0,0 +1,24 @@
+package com.spring.jpa_advanced.dao;
+
+import com.spring.jpa_advanced.entity.Course;
+import com.spring.jpa_advanced.entity.Instructor;
+import com.spring.jpa_advanced.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    public void save(Instructor instructor);
+    public Instructor findById(int id);
+    public void deleteById(int id);
+    public InstructorDetail findInstructorDetailById(int id);
+    public void deleteInstructorDetailById(int id);
+    public List<Instructor> findAll();
+    public List<Course> findCoursesByInstructorId(int id);
+    public Instructor findInstructorWithJoin(int id);
+    public Instructor update(Instructor instructor);
+    public Course update(Course course);
+    public Course findCourseById(int id);
+    public void deleteCourseById(int id);
+    public void saveCourse(Course course);
+    public Course getCourseWithReviewsById(int id);
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/04-one-to-many-uni/src/main/java/com/spring/jpa_advanced/dao/AppDAOImpl.java
@@ -0,0 +1,129 @@
+package com.spring.jpa_advanced.dao;
+
+import com.spring.jpa_advanced.entity.Course;
+import com.spring.jpa_advanced.entity.Instructor;
+import com.spring.jpa_advanced.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+    private final EntityManager entityManager;
+
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+
+    @Override
+    @Transactional
+    public Instructor findById(int id) {
+        return entityManager.find(Instructor.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        Instructor instructor = entityManager.find(Instructor.class, id);
+        List<Course> courses = instructor.getCourses();
+        for(Course course: courses){
+            course.setInstructor(null);
+        }
+
+        entityManager.remove(instructor);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int id) {
+        return entityManager.find(InstructorDetail.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int id) {
+        InstructorDetail instructorDetail = entityManager.find(InstructorDetail.class, id);
+
+        instructorDetail.getInstructor().setInstructorDetail(null);
+
+        entityManager.remove(instructorDetail);
+    }
+
+    @Override
+    public List<Instructor> findAll() {
+        TypedQuery<Instructor> query = entityManager.createQuery("from Instructor ", Instructor.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(int id) {
+        TypedQuery<Course> query = entityManager.createQuery("FROM Course WHERE instructor.id = :id", Course.class);
+        query.setParameter("id", id);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public Instructor findInstructorWithJoin(int id) {
+        TypedQuery<Instructor> query = entityManager.createQuery(
+          "FROM Instructor i " +
+                  "JOIN FETCH i.courses c " +
+                  "JOIN FETCH i.instructorDetail " +
+                  "WHERE i.id = :id",
+                Instructor.class
+        );
+
+        query.setParameter("id", id);
+
+        return query.getSingleResult();
+    }
+
+    @Override
+    @Transactional
+    public Instructor update(Instructor instructor) {
+        return entityManager.merge(instructor);
+    }
+
+    @Override
+    @Transactional
+    public Course update(Course course) {
+        return entityManager.merge(course);
+    }
+
+    @Override
+    public Course findCourseById(int id) {
+        return entityManager.find(Course.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCourseById(int id) {
+        Course course = entityManager.find(Course.class, id);
+        entityManager.remove(course);
+    }
+
+    @Override
+    @Transactional
+    public void saveCourse(Course course) {
+        entityManager.persist(course);
+    }
+
+    @Override
+    public Course getCourseWithReviewsById(int id) {
+        TypedQuery<Course> query = entityManager.createQuery(
+                "FROM Course c " +
+                "JOIN FETCH c.reviews " +
+                        "WHERE c.id = :id", Course.class);
+        query.setParameter("id", id);
+
+        return query.getSingleResult();
+    }
+}
